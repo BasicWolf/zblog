f6684e6241c6fc7ac7eb908020230deb889e163a
Zaur Nasibov <zaur@znasibov.info>
2023-02-25 16:08:07 +0200

    Fix Pelican configuration
    FEED_ALL_ATOM should contain a path to the generated Atom XML file

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -19,7 +19,7 @@ DATE_FORMATS = {
 }
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = True
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
